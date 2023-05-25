f507ce437f050847fecc5566f32504d914c1e0a9
timonschramm <timonschramm@t-online.de>
2023-05-25 09:12:12 +0200

    registration works fine

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,6 +2,10 @@
 // for information about these interfaces
 declare global {
 	namespace App {
+		interface Locals {
+			supabase: SupabaseClient<Database>;
+			getSession(): Promise<Session | null>;
+		  }
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
---
+++ src/routes/overview/+page.svelte
@@ -0,0 +1,1 @@
+<h2>Neue Seite</h2>
